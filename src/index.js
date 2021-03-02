0d9001f408a058916a5d4fabf779fcdaf38d7d58
bananabread08 <rmkfermin@gmail.com>
2021-03-02 22:50:43 +0800

    removed current dropdown, added hamburger icon, implemented mobile responsive UI

--- src/index.js
+++ src/index.js
@@ -1,35 +1,5 @@
-const aElements = document.querySelectorAll('a');
-const homeContent = document.querySelector('#home-content');
-const aboutContent = document.querySelector('#about-content');
-const clearActive = () => {
-  homeContent.style.display = 'none';
-  aboutContent.style.display = 'none';
-  aboutContent.classList.remove('active');
-  homeContent.classList.remove('active');
-  homeContent.parentElement.childNodes[1].innerText = 'Home v';
-  aboutContent.parentElement.childNodes[1].innerText = 'About v';
-};
-clearActive();
-aElements.forEach((a) => {
-  a.addEventListener('click', (e) => {
-    if (e.target.id === 'home') {
-      if (homeContent.style.display === 'none') {
-        clearActive();
-        e.target.innerText = 'Home ^';
-        homeContent.style.display = 'block';
-        homeContent.classList.toggle('active');
-      } else {
-        clearActive();
-      }
-    } else if (e.target.id === 'about') {
-      if (aboutContent.style.display === 'none') {
-        clearActive();
-        e.target.innerText = 'About ^';
-        aboutContent.style.display = 'block';
-        aboutContent.classList.toggle('active');
-      } else {
-        clearActive();
-      }
-    }
-  });
+const menuIcon = document.querySelector('.menu-toggle');
+const navi = document.querySelector('#navi');
+menuIcon.addEventListener('click', () => {
+  navi.classList.add('nav-active');
 });
