78a7a3d7321ec07196336e25842206f7511bb5bc
bananabread08 <rmkfermin@gmail.com>
2021-03-02 15:37:14 +0800

    more responsive Nav Bar, updated JS events/dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-const dropdown = document.querySelector('#dropdown');
-const dropdownContent = document.querySelector('.content');
-dropdownContent.style.display = 'none';
-
-dropdown.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (dropdownContent.style.display === 'none') {
-    dropdownContent.style.display = 'block';
-    dropdownContent.style.position = 'absolute';
-    dropdownContent.style.overflow = 'auto';
-    dropdownContent.style.zIndex = '1';
-    dropdownContent.style.marginLeft = '52%';
-  } else dropdownContent.style.display = 'none';
+const aElements = document.querySelectorAll('a');
+const homeContent = document.querySelector('#home-content');
+const aboutContent = document.querySelector('#about-content');
+const clearActive = () => {
+  homeContent.style.display = 'none';
+  aboutContent.style.display = 'none';
+  aboutContent.classList.remove('active');
+  homeContent.classList.remove('active');
+  homeContent.parentElement.childNodes[1].innerText = 'Home v';
+  aboutContent.parentElement.childNodes[1].innerText = 'About v';
+};
+clearActive();
+aElements.forEach((a) => {
+  a.addEventListener('click', (e) => {
+    if (e.target.id === 'home') {
+      if (homeContent.style.display === 'none') {
+        clearActive();
+        e.target.innerText = 'Home ^';
+        homeContent.style.display = 'block';
+        homeContent.classList.toggle('active');
+      } else {
+        clearActive();
+      }
+    } else if (e.target.id === 'about') {
+      if (aboutContent.style.display === 'none') {
+        clearActive();
+        e.target.innerText = 'About ^';
+        aboutContent.style.display = 'block';
+        aboutContent.classList.toggle('active');
+      } else {
+        clearActive();
+      }
+    }
+  });
 });
